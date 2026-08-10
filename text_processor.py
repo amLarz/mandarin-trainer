f198af778d1d8863b25b5f7a05820e8a0ad420e3
amLarz <larzroco@gmail.com>
2026-08-10 20:03:02 +0800

    refactor main.py
    text_processor takes text, POS, dep and lemma.

--- text_processor.py
+++ text_processor.py
@@ -5,6 +5,7 @@ nlp = spacy.load("en_core_web_sm")
 
 def process_text(text):
     doc = nlp(text)
+    
     for token in doc:
         print(token.text, token.pos_, token.dep_, token.lemma_)
         
