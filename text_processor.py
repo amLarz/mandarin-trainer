ffa54c14280cb1fcde63fe0a3a1af7b5ebb159bc
amLarz <larzroco@gmail.com>
2026-08-11 12:10:22 +0800

    feat load text from csv file

--- text_processor.py
+++ text_processor.py
@@ -1,12 +1,17 @@
 import spacy
+import csv
 
-# load the English language model
+# load the English NLP model
 nlp = spacy.load("en_core_web_sm")
 
-def process_text(text):
-    doc = nlp(text)
+def load_core_functional_words():
+    CORE_FUNCTIONAL_WORDS = set()
+    with open('core-functional-words-v1.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            word = row[0]
+            CORE_FUNCTIONAL_WORDS.add(word)
+            
+    return CORE_FUNCTIONAL_WORDS
+
     
-    for token in doc:
-        print(token.text, token.pos_, token.dep_, token.lemma_)
-        
-    return doc
