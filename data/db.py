72db6d5d158c8d79d5075b55dfa4b74d5b136889
amLarz <larzroco@gmail.com>
2026-08-16 18:07:43 +0800

    feat text processor is complete.
    feat initialyzing database's table words, sentences and words_sentences.

---
+++ data/db.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+con = sqlite3.connect('mandarin.db')
+cur = con.cursor()
+
+# WORDS TABLE
+cur.execute('''CREATE TABLE IF NOT EXISTS words (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    word TEXT NOT NULL,
+    frequency INTEGER NOT NULL DEFAULT 0
+)''')
+
+# SENTENCES TABLE
+cur.execute('''CREATE TABLE IF NOT EXISTS sentences (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    sentence TEXT NOT NULL
+)''')
+
+# WORDS_SENTENCES TABLE
+cur.execute('''CREATE TABLE IF NOT EXISTS words_sentences (
+    word_id INTEGER NOT NULL,
+    sentence_id INTEGER NOT NULL,
+    FOREIGN KEY (word_id) REFERENCES words(id),
+    FOREIGN KEY (sentence_id) REFERENCES sentences(id)
+)''')
+
+con.commit()
+
+def update_word_frequency(
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from transcribe import transcribe_audio
 from text_processor import process_text
-from frequency import update_database
-
 
 # transcribe audio and get result
 text = "I run everyday before going to school. 1, 2, 3, 4, 5" # TESTING PURPOSES: USE TRANSCRIBE_AUDIO()
 print("Transcribed Text:", text)
+
 # process the transcribed text
 processed_tiers = process_text(text)
 print("Processed Tiers:", processed_tiers)
