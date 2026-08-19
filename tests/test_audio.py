d194e4d89e054265b9dd5e39cd5badeb383350ff
amLarz <larzroco@gmail.com>
2026-08-19 18:33:18 +0800

    style name change test audio, prep for test rework.

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -4,7 +4,9 @@ import os
 FOLDER_PATH = os.path.dirname(os.path.abspath(__file__))
 
 def load_audio_files():
-    file_formats = (".mp3", ".wav", ".flac")
-    for file in os.listdir(os.path.join(FOLDER_PATH, 'audio-test')):
-        if file.endswith(file_formats):
-            return os.path.join(FOLDER_PATH, 'audio-test', file)
+    
+    valid_file_formats = (".mp3", ".wav", ".flac")
+    
+    for file in os.listdir(os.path.join(FOLDER_PATH)):
+        if file.endswith(valid_file_formats):
+            return os.path.join(FOLDER_PATH, file)
