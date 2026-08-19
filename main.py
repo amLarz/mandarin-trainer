63d33fa200ad67abb7e78bdec17096f6f8571e9e
amLarz <larzroco@gmail.com>
2026-08-19 20:37:51 +0800

    refactor remove uneeded imports
    initializing programm test
    
    refactor initialize changes for loading files to test each sentence.

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from audio.record import recording_audio
 from data.db import save_to_database
 from nlp.text_processor import process_text
 
-# TESTING IMPORT
-from tests.test_audio import load_audio_files
+def main():
+    # transcribe audio and get result
+    text = transcribe_audio(recording_audio()) # TESTING PURPOSES: USE TRANSCRIBE_AUDIO()
+    print("Transcribed Text:", text)
 
-# transcribe audio and get result
-text = transcribe_audio(recording_audio()) # TESTING PURPOSES: USE TRANSCRIBE_AUDIO()
-print("Transcribed Text:", text)
+    # process the transcribed text
+    processed_text = process_text(text)
+    print("Processed Text:", processed_text)
 
-# process the transcribed text
-processed_text = process_text(text)
-print("Processed Text:", processed_text)
-
-# update the word frequency in the database 
-print(save_to_database(processed_text))
+    # update the word frequency in the database 
+    print(save_to_database(processed_text))
+    
+if __name__ == "__main__":
+    main()
--- tests/audio_test.py
+++ tests/audio_test.py
@@ -1,4 +1,3 @@
-import whisper
 import os
 
 FOLDER_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -7,6 +6,9 @@ def load_audio_files():
     
     valid_file_formats = (".mp3", ".wav", ".flac")
     
-    for file in os.listdir(os.path.join(FOLDER_PATH)):
+    for file in os.listdir(os.path.join(FOLDER_PATH, "audio_files")):
         if file.endswith(valid_file_formats):
+            print(file)
             return os.path.join(FOLDER_PATH, file)
+        
+load_audio_files()
