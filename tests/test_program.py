1c2c47e8df73bad36f0e6920b3f1758085b507db
amLarz <larzroco@gmail.com>
2026-08-21 13:48:16 +0800

    fix 'or' problem in if statement

--- tests/test_program.py
+++ tests/test_program.py
@@ -4,6 +4,9 @@ from tests.load_test_files import load_files
 def test_main():
     audio_or_text = input("Do you want to test with audio files or text? (Enter 'audio' or 'text'): ").strip().lower()
     
+    if audio_or_text not in ['audio', 'text']:
+        print("Invalid input. Please enter 'audio' or 'text'.")
+        return 1  # Return an error code
     files = load_files(audio_or_text)
     print(files)
     i = 0
